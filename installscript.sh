5b1ef14c937568f16353a8fefec7d709adc060a2
Dr_Zhawitche <103216281+DrZhawitche@users.noreply.github.com>
2023-01-10 07:51:24 -0800

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -19,14 +19,14 @@ cd awesomewm-config
 mkdir ~/.config/awesome/
 cp rc.lua ~/.config/awesome/
 #Installing the required apps
-sudo pacman -S feh picom dmenu polkit-gnome volumeicon
+sudo pacman -S feh picom dmenu polkit-gnome volumeicon lxappearance
 #Copying the wallpapers 
 mkdir ~/wallpapers
-cp awesomewm-config/wallpapers/ ~/wallpapers/ -r
+cp /awesomewm-config/wallpapers/ ~/wallpapers/ -r
 
 # (free) Apps
 sudo pacman -S vim neovim alacritty libreoffice vlc zsh flameshot linux-headers virtualbox
-yay -S librewolf-bin archtweak-tool-git brave-bin oh-my-zsh-git zsh-completions-git zsh-synthax-highlighting-git 
+yay -S librewolf-bin archlinux-tweak-tool-git brave-bin oh-my-zsh-git zsh-completions-git zsh-synthax-highlighting-git 
 #Fun programs
 sudo pacman -S lolcat neofetch cmatrix misfortune
 yay -S cowsay-bin pfetch
@@ -34,14 +34,12 @@ yay -S cowsay-bin pfetch
 # (non-free) Apps
 sudo pacman -S discord
 
-#Vim/Nvim configuration
+#Vim configuration
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/DrZhawitche/vim-config.git
 cd vim-config
 cp .vimrc ~/
-mkdir ~/.config/nvim/
-cp .vimrc ~/.config/nvim/ && mv ~/.config/neovim/.vimrc ~/.config/nvim/init.vim
-vim -c :PlugInstall && nvim -c :PlugInstall
+vim -c :PlugInstall
 
 #Alacritty config
 
